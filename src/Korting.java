56637600347dd2abdabbacc30cce8a8e651c42be
Gebruiker <serdin13@outlook.com>
2021-06-26 14:14:22 +0200

    Uselles code verwijderd

--- src/Korting.java
+++ src/Korting.java
@@ -19,52 +19,6 @@ public class Korting{
                 '}';
     }
 
-    public String maandenOpties(){
-        String optie1 = "Minder dan 12 maanden";
-        String optie2 = "Meer dan 12 maanden";
-
-        if(klantMaanden < 12){
-            return optie1;
-        }else if(klantMaanden >= 12){
-            return optie2;
-        }
-        return maandenOpties();
-    }
-
-    public String reviewOpties(){
-        String optie1 = "Laag(<25)";
-        String optie2 = "Gemiddeld(>25)";
-
-        if(positieveReview < 25){
-            return optie1;
-        }else if(positieveReview > 25) {
-            return optie2;
-        }
-        return reviewOpties();
-    }
-
-
-    public String gekochteProductOpties(){
-        String optie1 = "Laag(<25)";
-        String optie2 = "Gemiddeld(>25)";
-
-        if(gekochteProduct < 25){
-            return optie1;
-        }else if(gekochteProduct >25){
-            return optie2;
-        }
-        return gekochteProductOpties();
-    }
-
-    public double decision(){
-        if(gekochteProduct < 25){
-            return 5.0;
-        }else if(gekochteProduct > 25){
-            return 25.0;
-        }
-        return decision();
-    }
-
     public boolean geefKortingControle() {
         if (klantMaanden > 12 && positieveReview > 0){
             return true;
